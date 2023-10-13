0577e3ae9e59cacc6467027fac93daffd4909480
ThousandDevils <c32flag@gmail.com>
2023-10-13 20:40:36 +0300

    added task36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -28,9 +28,10 @@ void PrintArray(int[] arr)
 int GetSumOddItems(int[] arr)
 {
     int sum = 0;
-    for (int i = 1; i < arr.Length; i++)
+
+    for (int i = 1; i < arr.Length; i += 2)
     {
-        if (i % 2 != 0) sum += arr[i];
+        sum += arr[i];
     }
     
     return sum;
